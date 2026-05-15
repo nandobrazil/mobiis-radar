bcbc256c801263348bc9ffabee774efcef50348a
Vinicius Soares <vinicius.santana@fretefy.onmicrosoft.com>
2026-05-15 17:23:46 -0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
-  providers: [provideAnimationsAsync(), provideRouter(routes)],
+  providers: [provideRouter(routes)],
 }).catch((error) => console.error(error));
